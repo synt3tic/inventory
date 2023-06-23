491df4c532bd01717e1cda4413aad6442fa69454
synt3tic <mel04nax@gmail.com>
2023-06-23 11:02:05 +0300

    ui-skelet-ready

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import UiInput from './components/UI/UiInput.vue';
-
-const inputValue = ref('22');
+import UiSkeletone from './components/UI/UiSkeletone.vue'
 </script>
 
 <template>
-  <UiInput v-model="inputValue" type="number" placeholder="Введите количество" />
+  <UiSkeletone />
 </template>
 
 <style lang="scss"></style>
---
+++ src/components/UI/UiSkeletone.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="ui-skeletone">
+    <div v-for="item in 7" class="ui-skeletone__stripe"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+$gradient: linear-gradient(to right, #3C3C3C, #444444, #333333);
+.ui-skeletone {
+  width: 190px;
+  height: 198px;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 16px;
+
+  &__stripe {
+    height: 10px;
+    border-radius: 4px;
+    justify-self: center;
+    background: $gradient;
+
+    &:nth-child(1) {
+      width: 190px;
+      height: 26px;
+      margin-bottom: 8px;
+      border-radius: 8px;
+    }
+
+    &:nth-child(2) {
+      width: 155px;
+    }
+
+    &:nth-child(3) {
+      width: 190px;
+    }
+
+    &:nth-child(4) {
+      width: 170px;
+    }
+
+    &:nth-child(5) {
+      width: 160px;
+    }
+
+    &:nth-child(6) {
+      width: 140px;
+    }
+
+    &:nth-child(7) {
+      width: 80px;
+    }
+  }
+}
+</style>
